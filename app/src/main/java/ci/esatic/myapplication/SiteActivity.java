03cca7999ac9d51d87b98206304f2d319b7b9f19
EmmanuelETOU <ephremfrancketou@gmail.com>
2023-10-08 22:52:35 +0000

    Timfao essaie modifié

---
+++ app/src/main/java/ci/esatic/myapplication/SiteActivity.java
@@ -0,0 +1,15 @@
+package ci.esatic.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SiteActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loc);
+
+    }
+}
